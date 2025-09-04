cf80fb00adad81fa7d99dfbe5edbb1705d35adfe
Y.Ouallal <2235702@talnet.nl>
2025-09-04 10:52:34 +0200

    Lijnen

---
+++ OpdrachtH3.1.java
@@ -0,0 +1,14 @@
+size(200,200);
+background(255,0,0);
+
+stroke(0,255,0);
+strokeWeight(5);
+line(10,10,190,190);
+
+stroke(255,255,255);
+strokeWeight(4);
+line(27,57,147,34);
+
+stroke(0,0,255);
+strokeWeight(6);
+line(50,75,157,19);
