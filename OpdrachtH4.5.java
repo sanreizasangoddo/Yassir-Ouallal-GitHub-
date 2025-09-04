5814b535a0a98381dc5963c31a429578aaafbbde
Y.Ouallal <2235702@talnet.nl>
2025-09-04 13:58:08 +0200

    Verdiepingsopdrachten

---
+++ OpdrachtH4.5.java
@@ -0,0 +1,16 @@
+float a = 42174575;
+float b = 3600;
+float c = 24;
+float d = 365;
+float totaal = 0;
+
+totaal = a / b;
+println(totaal);
+
+a = totaal;
+totaal = a / c;
+println(totaal);
+
+a = totaal;
+totaal = a / d;
+println(totaal);
---
+++ OpdrachtH4.6.java
@@ -0,0 +1,13 @@
+float a = 7.8;
+float b = 5.8;
+float c = 9.3;
+
+float gemiddeld = 0;
+
+gemiddeld = (a + b + c) / 3;
+println(gemiddeld);
+if (gemiddeld >= 6) {
+  println("Geslaagd");
+} else {
+  println("Niet geslaagd");
+}
