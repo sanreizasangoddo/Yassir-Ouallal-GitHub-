b7b1718a88e1a9d8fc728ecabd3b5f7b8d62e2e4
Y.Ouallal <2235702@talnet.nl>
2025-09-04 11:29:42 +0200

    Rekenen met getallen

---
+++ OpdrachtH4.2.java
@@ -0,0 +1,21 @@
+int a = 15;
+int b = 2;
+int c = 31;
+int d = 3;
+int e = 10;
+int f = 5;
+int g = 38;
+int h = 4;
+int totaal = 0;
+
+totaal = a % b;
+println(totaal);
+
+totaal = c % d;
+println(totaal);
+
+totaal = e % f;
+println(totaal);
+
+totaal = g % h;
+println(totaal);
