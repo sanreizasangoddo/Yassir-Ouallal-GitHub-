f190bdd81a8f83e69ea4a3e4a14d5c275582d06b
Y.Ouallal <2235702@talnet.nl>
2025-09-08 13:37:58 +0200

    Logische operator && en ||

---
+++ OpdrachtH6.5.java
@@ -0,0 +1,9 @@
+boolean schieten = false;
+boolean kogel = true;
+boolean herladen = true;
+
+if (schieten == true && kogel == true || kogel == true && herladen == true) {
+  println("Hit!");
+} else {
+  println("Miss!");
+}
