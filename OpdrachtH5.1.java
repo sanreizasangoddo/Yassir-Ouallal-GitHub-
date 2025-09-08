bdd0a4c165d591e7292798b4241a931828ee5171
Y.Ouallal <2235702@talnet.nl>
2025-09-08 12:41:31 +0200

    Spaties tussen String waardes

---
+++ OpdrachtH5.1.java
@@ -0,0 +1,9 @@
+String eenVariabele = "Hallo,";
+String nogEenVariabele = "mijn naam is";
+String anderVariabele = "Yassir Ouallal.";
+String nogEenAnderVariabele = "Ik ben zestien jaar.";
+String samenEenVariabele = "";
+samenEenVariabele = eenVariabele + " " + nogEenVariabele + " " + anderVariabele + " "
++ nogEenAnderVariabele + " ";
+
+println(samenEenVariabele);
