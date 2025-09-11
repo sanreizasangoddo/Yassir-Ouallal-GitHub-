9db5e17aec422dae15f802856dbfcbb11130f609
Y.Ouallal <2235702@talnet.nl>
2025-09-11 13:01:14 +0200

    Winnaar

---
+++ OpdrachtH6_9.java
@@ -0,0 +1,10 @@
+int speler1Score = 30;
+int speler2Score = 30;
+
+if (speler1Score > speler2Score) {
+  println("Speler 1 is de winnaar!");
+} else if (speler1Score < speler2Score) {
+  println("Speler 2 is de winnaar!");
+} else if (speler1Score == speler2Score) {
+  println("Het is gelijkspel...");
+}
