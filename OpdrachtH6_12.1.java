578ca01cdcc08de1b63986b1fc8d28fcedab5c0a
Y.Ouallal <2235702@talnet.nl>
2025-09-11 13:06:16 +0200

    Geslaagd of niet?

---
+++ OpdrachtH6_12.1.java
@@ -0,0 +1,10 @@
+float gevolgdeLessen = 16;
+float totaalAantalLessen = 20;
+float cijfer = 5.5;
+float percentageLessenGevolgd = (gevolgdeLessen / totaalAantalLessen);
+
+if (percentageLessenGevolgd >= 0.80 && cijfer >= 5.5) {
+  println("Geslaagd!");
+} else {
+  println("Gezakt...");
+} // Klaas is geslaagd.
---
+++ OpdrachtH6_12.java
@@ -0,0 +1,10 @@
+float gevolgdeLessen = 17;
+float totaalAantalLessen = 20;
+float cijfer = 7.0;
+float percentageLessenGevolgd = (gevolgdeLessen / totaalAantalLessen);
+
+if (percentageLessenGevolgd >= 0.80 && cijfer >= 5.5) {
+  println("Geslaagd!");
+} else {
+  println("Gezakt...");
+} // Jan is geslaagd.
