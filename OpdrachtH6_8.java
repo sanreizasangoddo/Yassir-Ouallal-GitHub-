049d105a2309a5b18815a69b23a63b5a8959d253
Y.Ouallal <2235702@talnet.nl>
2025-09-11 13:00:11 +0200

    Temperatuur

---
+++ OpdrachtH6_8.java
@@ -0,0 +1,8 @@
+int temperatuurCelsius = 28;
+if (temperatuurCelsius >= 25 && temperatuurCelsius < 30) {
+  println("warm");
+} else if (temperatuurCelsius >= 30) {
+  println("heet");
+} else if (temperatuurCelsius < 25) {
+  println("het is nu " + temperatuurCelsius + " graden");
+}
