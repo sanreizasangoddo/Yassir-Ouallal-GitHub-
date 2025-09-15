f84f19597061755a9d285fd35e657674019b11da
Y.Ouallal <2235702@talnet.nl>
2025-09-15 10:37:41 +0200

    Verdiepingsopdrachten H7

---
+++ OpdrachtH7.4.java
@@ -0,0 +1,25 @@
+int DagenvandeWeek = 1;
+
+switch(DagenvandeWeek) {
+case 1:
+  println("Maandag");
+  break;
+case 2:
+  println("Dinsdag");
+  break;
+case 3:
+  println("Woensdag");
+  break;
+case 4:
+  println("Donderdag");
+  break;
+case 5:
+  println("Vrijdag");
+  break;
+case 6:
+  println("Zaterdag");
+  break;
+case 7:
+  println("Zondag");
+  break;
+}
---
+++ OpdrachtH7.5.java
@@ -0,0 +1,26 @@
+int cijfer = 5;
+
+switch(cijfer) {
+case 1:
+case 2:
+case 3:
+  println("Slecht...");
+  break;
+case 4:
+  println("Onvoldoende...");
+  break;
+case 5:
+  println("Matig");
+  break;
+case 6:
+case 7:
+  println("Voldoende");
+  break;
+case 8:
+case 9:
+case 10:
+  println("Goed!");
+  break;
+  default:
+  println("Waarschuwing: Verkeerd cijfer ingevoerd!");
+}
